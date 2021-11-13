9e07afaac184c8714c4dd40386753880ea8ff034
Sebastien <SebastienFCT@users.noreply.github.com>
2021-11-13 11:19:31 -0600

    adds color to timer

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class TimerController : MonoBehaviour
 {
 
+    private float startingTimer = 60;
     private float timeRemaining = 60;
     private bool timerIsRunning = false;
     private Text label;
@@ -36,10 +37,33 @@ public class TimerController : MonoBehaviour
         float minutes = Mathf.FloorToInt(timeToDisplay / 60);
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
 
+        if (timeToDisplay <= 6)
+        {
+            label.color = Color.red;
+        }
+        else if (timeToDisplay > startingTimer * 60 / 100)
+        {
+            label.color = Color.blue;
+        }
+        else if (timeToDisplay > startingTimer * 40 / 100)
+        {
+            label.color = Color.green;
+        }
+        else
+        {
+            label.color = Color.yellow;
+        }
+
         label.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     // MARK: - Public exposure
+
+    public void initWithValue(float value)
+    {
+        startingTimer = value;
+        timeRemaining = value;
+    }
 
     public void SetTimeRemaining(float value)
     {
