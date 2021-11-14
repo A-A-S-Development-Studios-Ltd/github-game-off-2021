c16c9fbb50bfe29989d54448d58056dfb9bb6758
Sebastien <SebastienFCT@users.noreply.github.com>
2021-11-14 11:11:39 -0600

    adds sound

---
+++ Assets/Scripts/HomeController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HomeController : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
