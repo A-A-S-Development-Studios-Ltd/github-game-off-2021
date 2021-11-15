55c1916d5beb5fe87eca67ef4438d6069dab4811
Sebastien <SebastienFCT@users.noreply.github.com>
2021-11-14 19:26:09 -0600

    adds moving cloud to home screen

---
+++ Assets/Scripts/NewGameButtonController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameButtonController : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();    
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            audioSource.Play();
+        }
+    }
+}
