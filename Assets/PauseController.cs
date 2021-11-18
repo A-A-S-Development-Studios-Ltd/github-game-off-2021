1f82bb654c1062215309b72011ad08189caa8f72
Sebastien FOCK CHOW THO <sebastienfct@gmail.com>
2021-11-17 22:34:25 -0600

    pause feature

---
+++ Assets/PauseController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public void TriggerPause()
+    {
+        Time.timeScale = Time.timeScale == 1 ? 0 : 1;
+    }
+}
