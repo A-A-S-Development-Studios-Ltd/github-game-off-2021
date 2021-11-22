cd4d79ceb8d2f8b72e8a39bbd46fd0e00996a340
numerous6 <angel@aguilar.ca>
2021-11-21 22:31:43 -0600

    introducing bugs gradually

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -35,14 +35,37 @@ public class Level1 : MonoBehaviour
     }
     Wave GetRamdomWave()
     {
+        var max = waveCount;
+        if (max > 3)
+        {
+            max = waveCount % 3;
+        }
         Dictionary<Bug, int> bugs = new Dictionary<Bug, int>();
-        bugs.Add(ant, Random.Range(1, waveCount));
-        bugs.Add(bee, Random.Range(1, waveCount));
-        bugs.Add(beetle, Random.Range(1, waveCount));
-        bugs.Add(goldLadyBug, 2);
-        bugs.Add(fireAnt, Random.Range(1, 3));
-        bugs.Add(ladyBug, Random.Range(1, waveCount));
-        bugs.Add(stinkBug, Random.Range(1, waveCount));
+        if (waveCount > 0)
+        {
+            bugs.Add(ladyBug, Random.Range(1, max));
+        }
+        if (waveCount > 2)
+        {
+            bugs.Add(bee, Random.Range(1, max));
+        }
+        if (waveCount > 4)
+        {
+            bugs.Add(goldLadyBug, 2);
+            bugs.Add(beetle, Random.Range(1, max));
+        }
+        if (waveCount > 6)
+        {
+            bugs.Add(ant, Random.Range(1, max));
+        }
+        if (waveCount > 8)
+        {
+            bugs.Add(fireAnt, Random.Range(1, max));
+        }
+        if (waveCount > 10)
+        {
+            bugs.Add(stinkBug, Random.Range(1, max));
+        }
         return new Wave(bugs, waveCount);
     }
 }
