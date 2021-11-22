2f0ae13a211588ce0d73e23f8488ddac96362913
numerous6 <angel@aguilar.ca>
2021-11-22 00:47:49 -0600

    added spawn delays

--- Assets/Scripts/Wave.cs
+++ Assets/Scripts/Wave.cs
@@ -1,30 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
 public class Wave
 {
     public Dictionary<Bug, int> bugRegistry;
     List<Bug> bugList;
     int spawnRate;
-    public Wave(Dictionary<Bug, int> bugRegistry, int spawnRate)
+    int waveCount;
+    public Wave(Dictionary<Bug, int> bugRegistry, int waveCount)
     {
         BugEvents.onDeath += this.bugDead;
         bugList = new List<Bug>();
         this.bugRegistry = bugRegistry;
-        this.spawnRate = spawnRate;
+        this.spawnRate = 0;
+        this.waveCount = waveCount;
     }
-    public void StartWave()
+    public async void StartWave()
     {
-
         foreach (KeyValuePair<Bug, int> item in bugRegistry)
         {
-            bugList.AddRange(BugGenerator.generate(bug: item.Key, count: item.Value));
+            spawnRate = Random.Range(0, 3);
+            bugList.AddRange(await generateBugs(item.Key, item.Value));
         }
+    }
+    public async Task<List<Bug>> generateBugs(Bug bug, int count)
+    {
+        if (bugList.Count > 3)
+        {
+            await Task.Delay(spawnRate * 1000);
+        }
+        return BugGenerator.generate(bug: bug, count: count);
     }
     public void bugDead(Bug bug)
     {
         bugList.Remove(bug);
-        if (bugList.Count <= 2)
+        if (bugList.Count == 0)
         {
             WaveEvents.WaveComplete(this);
             BugEvents.onDeath -= this.bugDead;
