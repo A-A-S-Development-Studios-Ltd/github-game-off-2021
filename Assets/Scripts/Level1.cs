766b7dfe08496a1a185aff652732cbd28c41e57a
numerous6 <angel@aguilar.ca>
2021-11-25 19:25:08 -0600

    removed unused code

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -18,51 +18,15 @@ public class Level1 : MonoBehaviour
     public int waveCount;
     public int bugCount;
     public Text waveLabel;
-    public bool isInfiniteMode;
-    private int[] speedGears;
-    private float speedIncreaseInterval;
     void Start()
     {
         bugList = new List<Bug> { ant, bee, beetle, goldLadyBug, fireAnt, ladyBug, stinkBug, ant, bee, beetle, fireAnt, ladyBug, stinkBug, ladyBug, ant, beetle, ladyBug, ant, beetle };
-        speedGears = new int[] { 3, 2, 1 };
-        speedIncreaseInterval = 0.01f;
         WaveEvents.onComplete += this.OnWaveComplete;
         waveCount = 0;
         bugCount = 0;
         BugEvents.onDeath += OnSquish;
         //change infinite mode to change between modes
-        isInfiniteMode = false;
-        if (isInfiniteMode == false)
-        {
-            StartNextWave();
-        }
-    }
-    private void Update()
-    {
-        if (isInfiniteMode == true)
-        {
-            Bug bug = bugList[Random.Range(0, bugList.Count - 1)];
-            int count = Random.Range(1, 3);
-            int time = Mathf.FloorToInt(Time.time);
-            Debug.Log("playing infinite: " + time);
-            if (bugCount < 3)
-            {
-                BugGenerator.generate(bug: bug, count: count);
-                bugCount += count;
-                bug = bugList[Random.Range(0, bugList.Count - 1)];
-                BugGenerator.generate(bug: bug, count: count);
-                bugCount += count;
-            }
-            else if (bugCount > 3 && bugCount < 11)
-            {
-                int ranMod = Random.Range(2, 3);
-                if (time % ranMod == 0)
-                {
-                    BugGenerator.generate(bug: bug, count: count);
-                    bugCount += count;
-                }
-            }
-        }
+        StartNextWave();
     }
     public void OnSquish(Bug bug)
     {
