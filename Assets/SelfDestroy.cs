af6294ea821b61672713ba4ecf9350db787f7114
vajraJvalita <aafehr@gmail.com>
2021-12-08 05:20:45 -0600

    bug fixes

--- Assets/SelfDestroy.cs
+++ Assets/SelfDestroy.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class autoDestroy : MonoBehaviour
-{   
+public class SelfDestroy : MonoBehaviour
+{
     public float delayTime;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(this.gameObject, delayTime);
+        
     }
 }
